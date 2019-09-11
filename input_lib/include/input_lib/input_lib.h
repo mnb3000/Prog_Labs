30e55e771917ae9714cb22e2f50648ef9c4142e3
mnb3000 <misha.mykolyshyn@gmail.com>
2019-09-11 21:43:19 +0300

    Added inputInt function to input_lib

--- input_lib/include/input_lib/input_lib.h
+++ input_lib/include/input_lib/input_lib.h
@@ -1,4 +1,5 @@
 #ifndef PROG_LABS_INPUT_LIB_H
 #define PROG_LABS_INPUT_LIB_H
     extern double inputDouble(char message[], int doNegativeCheck);
+    extern int inputInt(char message[], int doNegativeCheck);
 #endif //PROG_LABS_INPUT_LIB_H
--- input_lib/src/input_lib.c
+++ input_lib/src/input_lib.c
@@ -17,3 +17,20 @@ double inputDouble(char message[], int doNegativeCheck) {
     }
     return out;
 }
+
+int inputInt(char message[], int doNegativeCheck) {
+    int out;
+    char str[256];
+    printf("%s", message);
+    scanf("%d", &out);
+    fgets(str, sizeof(str), stdin);
+    if (strlen(str) > 1) {
+        printf("Invalid input! Try again!\n");
+        return inputInt(message, doNegativeCheck);
+    }
+    if (doNegativeCheck > 0 && out < 0) {
+        printf("Input is negative! Try again!\n");
+        return inputInt(message, doNegativeCheck);
+    }
+    return out;
+}
