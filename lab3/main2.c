72caf5a53c2acb49749332eda0eb45afdb159ff7
mnb3000 <misha.mykolyshyn@gmail.com>
2019-09-12 00:31:45 +0300

    Added lab 3.2

---
+++ lab3/main2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "input_lib/input_lib.h"
+
+int inputIntAndCheckBoundaries(char *message, int doNegativeCheck, int notLessThan, int notGreaterThen) {
+    int res = inputInt(message, doNegativeCheck);
+    if (res < notLessThan || res > notGreaterThen) {
+        printf("Invalid boundaries!\n");
+        return inputIntAndCheckBoundaries(message, doNegativeCheck, notLessThan, notGreaterThen);
+    }
+    return res;
+}
+
+int getDayIndex(int date, int month, int year) {
+    int isLeapYear = 0;
+    if (((year % 100 != 0) && (year % 4 == 0)) || ((year % 100 == 0) && (year % 400 == 0))) {
+        isLeapYear = 1;
+    }
+    int n = 0;
+    if (month <= 2) {
+        if (isLeapYear == 1) {
+            n = 1;
+        } else {
+            n = 2;
+        }
+    }
+    return ((int) (365.25 * year) + (int) (30.56 * month) + date + n) % 7;
+}
+
+int main() {
+    int date, month, year;
+    char *days[7] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+    date = inputIntAndCheckBoundaries("Input date: ", 1, 1, 31);
+    month = inputIntAndCheckBoundaries("Input month: ", 1, 1, 12);
+    year = inputIntAndCheckBoundaries("Input year: ", 1, 1000, 9999);
+    int day = getDayIndex(date, month, year);
+    printf("%s", days[day]);
+}
