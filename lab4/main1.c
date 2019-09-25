0a76c5c4af4525c73e1ebe5aa60d7ed658e5e315
mnb3000 <misha.mykolyshyn@gmail.com>
2019-09-25 23:32:36 +0300

    Improved lab 4 docs (taylor sin still broken)

--- lab4/main1.c
+++ lab4/main1.c
@@ -48,7 +48,7 @@ double taylorCos(double x, double eps) {
 }
 
 /**
- * Prints line separator with fixed length
+ * Prints line separator for n columns
  * @param columns Number of columns
  */
 void printTableSeparator(int columns) {
@@ -64,9 +64,9 @@ void printTableSeparator(int columns) {
 /**
  * Prints table header
  * @param headers Array of header names
- * @param columns Number of columns
  */
-void printTableHeader(char **headers, int columns) {
+void printTableHeader(char **headers) {
+    int columns = sizeof(*headers);
     printTableSeparator(columns);
     for (int i = 0; i < columns; ++i) {
         printf("|%10s", headers[i]);
@@ -76,32 +76,41 @@ void printTableHeader(char **headers, int columns) {
 }
 
 /**
- * Prints table line with unfixed length
+ * Prints table line of double values with unfixed number of columns
  * @param values Array of values to print
  */
-void printTableLine(double *values) {
-    for (int i = 0; i < sizeof(values) - 1; ++i) {
+void printDoubleValueTableLine(double *values) {
+    int columns = sizeof(values);
+    for (int i = 0; i < columns - 1; ++i) {
         printf("|%10lf", values[i]);
     }
     printf("%s", "|\n");
 }
 
 int main() {
-    char *headers[] = {"Argument", "Taylor sin", "Sin", "Sin diff", "Taylor cos", "Cos", "Cos diff"};
+    char *headers[] = {"Argument", "Taylor sin", "Sin", "Sin diff", "Taylor cos", "Cos", "Cos diff"}; // Table header array
     printf("Input all values IN DEGREES!\n");
+    // Data input
     double x1 = inputDouble("Enter x1: ", 0, 0);
     double x2 = inputDouble("Enter x2: ", 0, 0);
     double dx = inputDouble("Enter dx: ", 0, 0);
     double eps = inputDouble("Enter epsilon: ", 0, 0);
-    printTableHeader(headers, 7);
+    printTableHeader(headers);
+    // x Initial value
     double x = x1;
-    while (x < x2) {
+    while (x <= x2) {
         double radX = degToRad(x);
+        /**
+         * Array of values for printTableLine function
+         * Consists of 7 elements for each column of table as declared on headers[] array
+         */
         double values[7];
+        // Value calculation
         double tSin = taylorSin(radX, eps);
         double rSin = sin(radX);
         double tCos = taylorCos(radX, eps);
         double rCos = cos(radX);
+        // Value array fill
         values[0] = x; // Argument
         values[1] = tSin; // Taylor sin
         values[2] = rSin; // Math.h sin
@@ -109,7 +118,9 @@ int main() {
         values[4] = tCos; // Taylor cos
         values[5] = rCos; // Math.h cos
         values[6] = rCos - tCos; // Cos difference
-        printTableLine(values);
+        // Data output
+        printDoubleValueTableLine(values);
+        // X increment
         x += dx;
     }
 }
