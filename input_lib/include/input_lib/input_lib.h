72fa3bd53d2a7db132b80fe10673ffc42af3efaf
mnb3000 <misha.mykolyshyn@gmail.com>
2020-01-09 06:16:32 +0200

    Input lib header fix

--- input_lib/include/input_lib/input_lib.h
+++ input_lib/include/input_lib/input_lib.h
@@ -4,6 +4,6 @@
     extern int inputInt(char message[], int doNegativeCheck, int doZeroCheck);
     extern int inputIntMin(char message[], int doNegativeCheck, int doZeroCheck, int min);
     extern int inputIntMax(char message[], int doNegativeCheck, int doZeroCheck, int max);
-    extern int inputIntMinMax(char message[], int doNegativeCheck, int doZeroCheck, int min, int max);
+    extern int inputIntMinMax(char message[], int min, int max);
     extern void inputString(char message[], char* string, size_t len);
 #endif //PROG_LABS_INPUT_LIB_H
